125baeea7c5e2482550034cb25c29876188e0977
IcEternal <devillaw_zhc@163.com>
2013-07-03 22:07:37 +0800

    add design-doc & develop.log

--- application/views/includes/footer.php
+++ application/views/includes/footer.php
@@ -3,7 +3,6 @@
 <script src="<?php echo base_url() ?>public/js/jquery-1.9.1.min.js"></script>
 <script src="<?php echo base_url() ?>public/js/bootstrap.min.js"></script>
 
-
 <script>
 	$(function (){
 		$.ajax({
