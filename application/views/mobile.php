85788bf526ba01eb0ba0cc10ddd1f621d9e8f100
IcEternal <devillaw_zhc@163.com>
2013-09-07 21:49:38 +0800

    add share button after order

--- application/views/mobile.php
+++ application/views/mobile.php
@@ -7,9 +7,9 @@
 	<link rel="stylesheet" href="<?php echo base_url() ?>public/css/bootstrap.min.css">
 </head>
 <body>
-	您正在使用手机浏览，若使用移动流量模式可能因为图片产生较多流量。<br />
-	如果您是闵行校区学生，可以去 <strong>图书馆</strong> 或者 <strong>东下院东中院</strong>，都能用 <strong>jAccount</strong>连无线哈~<br />
-	若要继续访问本站请
-	<a href="<?php echo site_url() ?>">点击此处</a>
+	<p>您正在使用手机浏览，若使用移动流量模式可能因为图片产生较多流量。</p>
+	<p>如果您是闵行校区学生，可以去 <strong>图书馆</strong> 或者 <strong>东下院东中院</strong>，都能用 <strong>jAccount</strong>连无线哈~</p>
+	<p>若要继续访问本站请
+	<a href="<?php echo site_url() ?>">点击此处</a></p>
 </body>
 </html>
