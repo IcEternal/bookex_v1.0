55143522c68e2989edb34db8ccda787b7b45fae8
IcEternal <devillaw_zhc@163.com>
2013-09-08 09:00:34 +0800

    add advertisement to share

--- application/views/index.php
+++ application/views/index.php
@@ -161,7 +161,7 @@ $(document).ready(function(){
 <script type="text/javascript" id="bdshell_js"></script> 
 <script type="text/javascript">
 	//在这里定义bds_config
-	var bds_config = {'bdText':'BookEx - 交大校内二手书交易平台。网站今年5月上线，现在共有近2000本书，价格十分优惠~还有工作人员免费送书~学长学姐说不定还会留下精致的笔记哈~快来看看吧','bdDesc':'交大校内二手书交易网站','bdComment':'以后有想卖掉的书就可以传在BookEx上咯，很多教材也能在平台上轻松找到哦~!','bdPic':'<?php echo base_url() ?>public/img/logo.jpg'};
+	var bds_config = {'bdText':'BookEx - 交大校内二手书交易平台。网站今年5月上线，现在共有近2000本书，价格十分优惠~还有工作人员免费送书~学长学姐说不定还会留下精致的笔记哈~快来看看吧','bdDesc':'交大校内二手书交易网站','bdComment':'以后有想卖掉的书就可以传在BookEx上咯，很多教材也能在平台上轻松找到哦~!','bdPic':'<?php echo base_url() ?>public/img/advertise.png'};
 	document.getElementById("bdshell_js").src = "http://bdimg.share.baidu.com/static/js/shell_v2.js?cdnversion=" + Math.ceil(new Date()/3600000);
 </script>
 <!-- Baidu Button END -->
