a7ab7e650c0e3a9f523ee5a735c07c04099ca619
ZHC <devillaw_zhc@163.com>
2013-10-04 15:52:18 +0800

    add generate tickets

--- application/controllers/test.php
+++ application/controllers/test.php
@@ -22,4 +22,41 @@ class Test extends CI_Controller {
 		echo "total sold:  ".$tot."!!<br/>";
 		echo "total sold:  ".$num."books!";
 	}
+	/*
+	function generate_ticket() {
+		$arr = array();
+		$max = 2000000000;
+		//discount_ticket
+		for ($i = 0;$i < 5000;$i++) {
+			$x = rand(0, $max);
+			while (isset($arr["$x"])) {
+				$x = rand(0, $max);
+			}
+			$arr["$x"] = true;
+			$str = (string)dechex($x);
+			$data = array(
+				'ticket_id' => $str,
+				'activated' => false,
+				'used' => false
+				);
+			$this->db->insert('discount_ticket', $data);
+		}
+
+		//free_ticket
+		for ($i = 0;$i < 1000;$i++) {
+			$x = rand(0, $max);
+			while (isset($arr["$x"])) {
+				$x = rand(0, $max);
+			}
+			$arr["$x"] = true;
+			$str = (string)dechex($x);
+			$data = array(
+				'ticket_id' => $str,
+				'activated' => false,
+				'used' => false
+				);
+			$this->db->insert('free_ticket', $data);
+		}
+	}
+	*/
 }
