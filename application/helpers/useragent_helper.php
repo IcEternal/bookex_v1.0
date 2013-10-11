bd43190c4101588831d7917b77d0a054d9a1fbd0
IcEternal <devillaw_zhc@163.com>
2013-10-11 17:41:49 +0800

    add send mail function in helper

--- application/helpers/useragent_helper.php
+++ application/helpers/useragent_helper.php
@@ -6,3 +6,29 @@ function isMobile() {
 	return true;
 	return false;
 }
+
+function send_mail($to,$title,$content)
+{
+	$ci = &get_instance();
+	$ci->load->library('email');            //加载CI的email类
+	
+	//以下设置Email参数
+	$config['protocol'] = 'smtp';
+	$config['smtp_host'] = 'smtp.163.com';
+	$config['smtp_user'] = 'bookex';
+	$config['smtp_pass'] = '2013bookexchange';
+	$config['smtp_port'] = '25';
+	$config['charset'] = 'utf-8';
+	$config['wordwrap'] = TRUE;
+	$config['mailtype'] = 'html';
+	$ci->email->initialize($config);
+	
+	//以下设置Email内容
+	$ci->email->from('tangqi00mail@163.com', 'fanteathy');
+	$ci->email->to($to);
+	$ci->email->subject($title);
+	$ci->email->message($content);
+
+	$ci->email->send();
+	//echo $ci->email->print_debugger();
+}
