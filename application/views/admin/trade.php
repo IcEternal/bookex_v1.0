9eea5072019814517b3d9413582be0c379edc8f0
ZHC <devillaw_zhc@163.com>
2013-10-25 21:16:26 +0800

    fix a bug for redeclaration of the function

--- application/views/admin/trade.php
+++ application/views/admin/trade.php
@@ -1,5 +1,14 @@
 <?php $this->load->view('includes/header') ?>
+<?php
 
+				function getColorByStr($str){
+					if (strpos($str, '正在取书')) return '#99FF00';
+					elseif (strpos($str, '送到易班')) return '#FF0000';
+					elseif (strpos($str, '正在送书')) return '#999900';
+					elseif (strpos($str, '找不到')) return '#333333';
+					else return '#3A87AD';
+				}
+?>
 	<div class="container">
 		
 		<div class="alert alert-error fade in">
@@ -84,13 +93,6 @@
 					$free_color = "class='error'";
 				}
 
-				function getColorByStr($str){
-					if (strpos($str, '正在取书')) return '#99FF00';
-					elseif (strpos($str, '送到易班')) return '#FF0000';
-					elseif (strpos($str, '正在送书')) return '#999900';
-					elseif (strpos($str, '找不到')) return '#333333';
-					else return '#3A87AD';
-				}
 				?>
 				<tr <?php echo $free_color;?>>
 		  			<td  width=20%>
