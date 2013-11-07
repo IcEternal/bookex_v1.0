88c243d2a8ac4ca0e7b6c2f047b52d3a957a0bf8
IcEternal <devillaw_zhc@163.com>
2013-11-07 18:16:29 +0800

    add activity decription

---
+++ application/views/act_double11.php
@@ -0,0 +1,38 @@
+<?php
+/*
+================================================================
+act_detail.php
+
+include: includes/header.php
+		 includes/footer.php
+
+The page of the activity details
+
+Whenever you changed this page, please leave a log here.
+The log includes time and changed content.
+Just like the following:
+
+#---------------------------------------------------------------
+#Last updated: 11.1.2013 by Wang Sijie
+#What's new: The first vision.
+================================================================
+ */ 
+?>
+
+<?php include("includes/header.php"); ?>
+
+<div class="content-full"><!-- structure -->
+	<h2>双11活动介绍</h2>
+	<p>双十一，你肯定想和妹子，好基友，好闺蜜一起度过~~<br/>
+		但是缺少一个人帮您提供精美的礼品？<br/>
+		或者缺少一个人送出迟迟不敢送出的礼品<br/>
+		博易BookEx为您精心准备了超级便宜实惠的崭新的小商品!!!<br/>
+		还免费赠送兑书券、精美的贺卡（需自行填写）和代送服务哦~
+	</p>
+	<br/>
+	<p>这里的商品比淘宝还便宜有木有很激动，这里的商品每件只有两个，都是全新的呢！可以送出属于你的独特审美。</p>
+	<br/>
+	<p>点击预订，和我们的工作人员联系吧~提出属于你的要求哦！我们将会在10日下午和晚上统一发货。</p>
+</div><!-- content-full -->
+
+<?php include("includes/footer.php"); ?>
